66c06c8c79c172c32c37d68dd96b45b2efc58197
Alfredo G Ceballos <agceballos@cpp.edu>
2017-10-19 10:45:02 -0700

    Finished

--- Ex3Client.java
+++ Ex3Client.java
@@ -4,16 +4,17 @@
 *	Excercise 3
 *	Professor Nima Davarpanah
 *****************************************/
+
 import java.io.OutputStream;
 import java.io.PrintStream;
 import java.io.InputStream;
-import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.ByteBuffer;
 
 public class Ex3Client {
 	public static void main(String[] args) {
 		try(Socket socket = new Socket("18.221.102.182", 38103)) {
+			
 			// Setting up input and output streams
 			InputStream in = socket.getInputStream();
 			OutputStream out = socket.getOutputStream();
@@ -26,9 +27,16 @@ public class Ex3Client {
 			int numBytes = 0 | in.read();
 			System.out.printf("Reading %d bytes:%n", numBytes);
 			
-			// Byte array set up to hold byte data
-			byte[] bytes = new byte[numBytes];
+			// Byte array set up to hold byte data. If the number
+			// of bytes to be read is odd, then the byte array size is // increased by 1 to even it out.
+			byte[] bytes = null;
+			if(numBytes % 2 != 0) {
+				bytes = new byte[numBytes + 1];
+			} else {
+				bytes = new byte[numBytes];
+			}
 			System.out.printf("Data received:%n");
+			
 			// Reading in bytes, saving them into array, and printing
 			for(int i = 0; i < numBytes; i++) {
 				if(i % 10 == 0){System.out.print("   ");}
@@ -59,54 +67,40 @@ public class Ex3Client {
 		} catch(Exception e){
 			System.out.println("It broke...");
 			e.printStackTrace();
-		}
+		}// End of try catch
 		
 	}// End of main
 	
 	public static short checksum(byte[] b) {
-		int total = 0; // Integers are 32 bits in Java
-		short sum = 0; // Shorts are 16 bits in Java
+		int intTotal = 0; // Integers are 32 bits in Java
 		byte zeroByte = 0;
 		
 		for(int i = 0; i < b.length; i += 2) {
 			// Taking in 2 sets of 8 bits in order to create a 
 			// 16 bit number
-			short intTemp = b[i];
-			intTemp = (short)(intTemp << 8);
-			intTemp += b[i];
+			int intTemp = b[i];
 			
-			// If b has an odd number of elements and is at 
-			// the last element
-			if(b.length % 2 == 1 && i + 1 == b.length){
-				System.out.println("Odd number detected");//delete
-				
-				if(total + intTemp < 0) {
-					System.out.println("Overflow detected?? Works???");
-					//total = total
-				}
-				
-				sum += (b[i] + zeroByte);
-				break; // Last thing to do in this case is break loop
-			} 
+			// First 8 bits already saved in temp value,
+			// shift over 8 bits to make room for next 8
+			intTemp = (intTemp << 8) & 0xFF00;
+			intTemp += (b[i + 1] & 0xFF);
 			
+			// Add those 16 bits to the total sum
+			intTotal += intTemp;
+
 			/**
 			 * If adding a 16 bit number to the current total triggers 
-			 * an overflow, then a negative number should be generated
-			 * since primitives are signed in Java
+			 * an overflow, wrap around
 			 */
-			if(total + intTemp < 0) {
-				System.out.println("Overflow detected?? Works???");
+			if((intTotal & 0xFFFF0000) > 0) {
+				intTotal &= 0xFFFF;
+				intTotal++;
 			}
-			
-			// Otherwise just add it to the total
-			total += intTemp;
-			
-			// if (triggers overflow)
-			// take upper 8 bits 
-			// lower 8 bits
-			//+upper 8 bits
-			// = sum
-		}
-		return sum;
+		} // End of for loop
+		
+		// Taking the total, converting it to its 1's complement
+		// and casting that value to a short value
+		return (short)(~(intTotal & 0xFFFF));
 	}// End of checksum
+
 }
